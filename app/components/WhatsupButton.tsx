8b17d0791d4b0922196bfff58b458680f0d68515
SayidRadhin <pmradhin@gmail.com>
2026-02-26 15:46:57 +0530

    updated whatsapp number

--- app/components/WhatsupButton.tsx
+++ app/components/WhatsupButton.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const WhatsupButton: React.FC = () => {
   return (
 	  <Link
-        href="https://wa.me/918891296161" // CHANGE THIS TO YOUR ADMIN WHATSAPP NUMBER
+        href="https://wa.me/917593855313" // CHANGE THIS TO YOUR ADMIN WHATSAPP NUMBER
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-4 right-4 bg-emerald-600 text-white py-4 px-5 opacity- rounded-full shadow-lg hover:bg-slate-600 transition flex items-center"
